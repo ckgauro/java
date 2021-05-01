10ad57778afe3d8b0ae61192396378921609ed41
Chandra kishor Gauro <ckgauro@gmail.com>
2021-04-30 22:41:08 -0400

    Init Prallel

---
+++ parallel-asynchronous-using-java/src/main/java/com/gauro/demo/forkjoin/StringTransformExample.java
@@ -0,0 +1,32 @@
+package com.gauro.demo.forkjoin;
+
+import com.gauro.demo.util.DataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.gauro.demo.util.CommonUtil.delay;
+import static com.gauro.demo.util.CommonUtil.stopWatch;
+import static com.gauro.demo.util.LoggerUtil.log;
+
+/**
+ * @author Chandra
+ */
+public class StringTransformExample {
+    public static void main(String[] args) {
+        stopWatch.start();
+        List<String> resultList=new ArrayList<>();
+        List<String> names= DataSet.namesList();
+        log("names :"+names);
+        names.forEach((name)->{
+            String newValue=addNameLengthTransform(name);
+            resultList.add(newValue);
+        });
+
+
+    }
+    private static String addNameLengthTransform(String name){
+        delay(500);
+        return name.length()+" - "+name;
+    }
+}
--- parallel-asynchronous-using-java/src/main/java/com/gauro/demo/service/InventoryService.java
+++ parallel-asynchronous-using-java/src/main/java/com/gauro/demo/service/InventoryService.java
@@ -1,7 +1,25 @@
 package com.gauro.demo.service;
 
+import com.gauro.demo.domain.Inventory;
+import com.gauro.demo.domain.ProductOption;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.gauro.demo.util.CommonUtil.delay;
+
 /**
  * @author Chandra
  */
 public class InventoryService {
+    public Inventory addInventroy(ProductOption productOption){
+        delay(500);
+        return Inventory.builder().count(2).build();
+    }
+    public CompletableFuture<Inventory> addInventory_CF(ProductOption productOption){
+        return CompletableFuture.supplyAsync(()->{
+            delay(500);
+            return Inventory.builder().count(2).build();
+        });
+    }
+
 }
