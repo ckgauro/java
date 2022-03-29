38b4c4d9396b7765718af97b46103f60a25e5f47
Chandra kishor Gauro <ckgauro@gmail.com>
2022-03-29 04:11:14 -0400

    java 7

--- Java 7 & Java 8 new features with Lambdas & Streams/Java7Example/src/main/java/com/gauro/java7/CatchingMultipleExceptions.java
+++ Java 7 & Java 8 new features with Lambdas & Streams/Java7Example/src/main/java/com/gauro/java7/CatchingMultipleExceptions.java
@@ -1,5 +1,64 @@
-package com.gauro.java7;/**
-*
-*@author Chandra
-*/public class CatchingMultipleExceptions {
+package com.gauro.java7;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Chandra
+ */
+public class CatchingMultipleExceptions {
+    private final static Logger LOGGER=java.util.logging.Logger.getLogger(CatchingMultipleExceptions.class.getName());
+
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        beforeJava7();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("With Java 7====>");
+        withJava7();
+        
+    }
+
+    /**
+     * Sample implementation before Java 7
+     *
+     */
+    private static void beforeJava7() {
+       // int arr[]={1,2,3};
+        int arr[]=null;
+        try{
+            for(int i=0;i<arr.length+1;i++){
+                System.out.println(arr[i]);
+            }
+        }catch (NullPointerException nex){
+            LOGGER.log(Level.SEVERE,nex.toString());
+        }catch (ArrayIndexOutOfBoundsException aioex){
+            LOGGER.log(Level.SEVERE,aioex.toString());
+        }
+    }
+
+    /**
+     * Sample implementation from Java 7
+     *
+     */
+    private static void withJava7() {
+       // int arr[]={1,2,3};
+        int arr[]=null;
+        try{
+            for(int i=0;i<arr.length+1;i++){
+                System.out.println(arr[i]);
+            }
+        }catch (NullPointerException | ArrayIndexOutOfBoundsException nex){
+           // nex.addSuppressed(new Throwable("Data issue"));
+            LOGGER.log(Level.SEVERE,nex.toString());
+        }
+    }
+
+
 }
