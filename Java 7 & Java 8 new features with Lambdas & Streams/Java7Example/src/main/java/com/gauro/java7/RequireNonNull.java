3e4d89645d58303991147fe58b416896a9d2f170
Chandra kishor Gauro <ckgauro@gmail.com>
2022-03-31 06:26:35 -0400

    Java7 Example

--- Java 7 & Java 8 new features with Lambdas & Streams/Java7Example/src/main/java/com/gauro/java7/RequireNonNull.java
+++ Java 7 & Java 8 new features with Lambdas & Streams/Java7Example/src/main/java/com/gauro/java7/RequireNonNull.java
@@ -1,7 +1,49 @@
 package com.gauro.java7;
 
+import java.util.Objects;
+
 /**
  * @author Chandra
  */
 public class RequireNonNull {
+
+    public static void main(String[] args) {
+        Person person=null;
+        processPersonDetails(person);
+
+//        Person person=new Person();
+//        person.setFirstName("Ram");
+//        person.setLastName("Das");
+//        processPersonDetails(person);
+
+    }
+
+    private static void processPersonDetails(Person person) {
+        Objects.requireNonNull(person,"Person Object can't be null");
+        System.out.println(person.getFirstName());
+        System.out.println(person.getLastName());
+    }
+
+    public static class Person{
+        private String firstName;
+        private String lastName;
+
+
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    }
 }
